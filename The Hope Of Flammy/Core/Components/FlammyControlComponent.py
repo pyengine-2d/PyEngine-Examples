76ebba3966a0d138f73748a1ee1c706684337863
LavaPower <lavapower84@gmail.com>
2019-05-11 20:55:25 +0200

    THOF : Flammy can shoot

--- The Hope Of Flammy/Core/Components/FlammyControlComponent.py
+++ The Hope Of Flammy/Core/Components/FlammyControlComponent.py
@@ -1,22 +1,37 @@
-from pyengine.Components import ControlComponent
+from pyengine.Components import ControlComponent, SpriteComponent, PositionComponent
 from pyengine.Enums import ControlType, Controls
-from pyengine.Components import SpriteComponent
+from pyengine import const
+
+from Core.Others.FireShoot import FireShoot
 
 
 class FlammyControlComponent(ControlComponent):
     def __init__(self):
         super(FlammyControlComponent, self).__init__(ControlType.FOURDIRECTION)
-        print("test")
+        self.vers = [0, 1]
+        self.shoottimer = 0
 
     def keypress(self, evt):
         super(FlammyControlComponent, self).keypress(evt)
         sprite = self.entity.get_component(SpriteComponent)
         if evt.key == self.controles[Controls.UPJUMP]:
             sprite.set_sprite("Images/Flammy/FlammyH.png")
+            self.vers = [0, -1]
         elif evt.key == self.controles[Controls.LEFT]:
             sprite.set_sprite("Images/Flammy/FlammyG.png")
+            self.vers = [-1, 0]
         elif evt.key == self.controles[Controls.DOWN]:
             sprite.set_sprite("Images/Flammy/Flammy.png")
+            self.vers = [0, 1]
         elif evt.key == self.controles[Controls.RIGHT]:
             sprite.set_sprite("Images/Flammy/FlammyD.png")
+            self.vers = [1, 0]
+        elif evt.key == const.K_SPACE and self.shoottimer <= 0:
+            shoot = FireShoot(self.vers, self.entity.get_component(PositionComponent).get_position(),
+                              self.entity.game)
+            self.entity.system.add_entity(shoot)
+            self.shoottimer = 10
 
+    def update(self):
+        super(FlammyControlComponent, self).update()
+        self.shoottimer -= 1
---
+++ The Hope Of Flammy/Core/Others/FireShoot.py
@@ -0,0 +1,38 @@
+from pyengine import Entity
+from pyengine.Components import PositionComponent, SpriteComponent, PhysicsComponent, MoveComponent
+
+
+class FireShoot(Entity):
+    def __init__(self, direction, position, game):
+        super(FireShoot, self).__init__()
+
+        self.game = game
+
+        if direction == [1, 0]:
+            self.add_component(PositionComponent([position[0]+32, position[1]]))
+            self.add_component(SpriteComponent("Images/Tirs/TirD.png"))
+        elif direction == [-1, 0]:
+            self.add_component(PositionComponent([position[0]-32, position[1]]))
+            self.add_component(SpriteComponent("Images/Tirs/TirG.png"))
+        elif direction == [0, 1]:
+            self.add_component(PositionComponent([position[0], position[1]+40]))
+            self.add_component(SpriteComponent("Images/Tirs/TirB.png"))
+        elif direction == [0, -1]:
+            self.add_component(PositionComponent([position[0], position[1]-32]))
+            self.add_component(SpriteComponent("Images/Tirs/TirH.png"))
+        phys = self.add_component(PhysicsComponent(False))
+        phys.set_callback(self.collision)
+        self.add_component(MoveComponent(direction))
+
+    def collision(self, obj, cause):
+        if obj in self.game.ennemies:
+            self.game.entitysystem.entities.remove(obj)
+            self.game.entitysystem.entities.remove(self)
+            del self.game.ennemies[self.game.ennemies.index(obj)]
+            if len(self.game.ennemies) == 0:
+                self.game.door.open_door()
+        elif type(obj) == type(self):
+            self.game.entitysystem.entities.remove(self)
+            self.game.entitysystem.entities.remove(obj)
+        elif obj != self.game.flammy:
+            self.game.entitysystem.entities.remove(self)
