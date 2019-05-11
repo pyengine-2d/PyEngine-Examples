12d0d1ff72c72928ac779cd0c96b90c728d52ae2
LavaPower <lavapower84@gmail.com>
2019-05-11 20:52:42 +0200

    THOF : Add states in window

--- The Hope Of Flammy/The Hope Of Flammy.py
+++ The Hope Of Flammy/The Hope Of Flammy.py
@@ -2,15 +2,21 @@ from pyengine import Window
 
 from Core.Menu import Menu
 from Core.Game import Game
+from Core.Win import Win
+from Core.Loose import Loose
 
 window = Window(640, 480, (255, 255, 255))
 window.set_title("The Hope Of Flammy")
 
 menu = Menu()
 jeu = Game()
+win = Win()
+loose = Loose()
 
 window.add_state(jeu)
 window.add_state(menu)
+window.add_state(win)
+window.add_state(loose)
 
 window.set_current_state("Menu")
 window.run()
